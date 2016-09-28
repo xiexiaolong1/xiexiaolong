b7e0039f2af0a8833e4cf8c40c53505c7a712483
xiexiaolong1 <715155045@qq.com>
2016-09-28 22:25:15 +0800

    谢晓龙的实验

---
+++ hello/src/main/java/cn/hylstudio/android/HelloWorld/HelloWorldActivity.java
@@ -0,0 +1,21 @@
+package cn.hylstudio.android.HelloWorld; /**
+ * created by hyl.
+ * 1：编程项目-[上机实践]在Android Studio中创建Hello World程序-具体内容
+ * 2：编程项目-[上机实践]学会使用Logcat调试程序-具体内容
+ */
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    public static String tag_test = "Practice Program by hyl";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(cn.hylstudio.android.helloworld.R.layout.activity_hello_world);
+        Log.d(tag_test, "test logcat");
+    }
+}
